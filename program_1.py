660c466f705e66a774dd07c39b8fa9f9da690957
Npeterson20 <Npeterson@students.unwsp.edu>
2024-11-01 01:33:56 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -3,11 +3,16 @@
 # (Use the included example file names.txt) and exists on the computer's disk.
 # Write a program that displays the number of names that are stored in the file.
 
-def count_file_lines():
-    ######################
-    # Add your code here #
-    ######################
-    print('In the count_file_lines function')
+def count_names_in_file():
+    try:
+        with open("names.txt", "r") as file:
+            names = file.readlines()
+            name_count = len(names)
+            print(f"The number of names in the file is: {name_count}")
+    except IOError:
+        print("An error occurred while trying to read the file.")
+
+count_names_in_file()
 
 
 
